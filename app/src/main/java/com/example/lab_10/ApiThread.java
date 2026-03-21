42588e35923617a602afabd041a53bca56b77cf3
Kai3234 <khai2892005@gmail.com>
2026-03-21 18:24:31 +0700

    quote

---
+++ app/src/main/java/com/example/lab_10/ApiThread.java
@@ -0,0 +1,64 @@
+package com.example.lab_10;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ApiThread extends Thread {
+
+    MainActivity activity;
+
+    public ApiThread(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            String apiUrl = "https://dummyjson.com/quotes/random";
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            InputStream inputStream = connection.getInputStream();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputStream)
+            );
+
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while((line = reader.readLine()) != null){
+                response.append(line);
+            }
+
+            reader.close();
+
+            JSONObject quoteObject = new JSONObject(response.toString());
+
+            String quote = quoteObject.getString("quote");
+            String author = quoteObject.getString("author");
+
+
+            activity.runOnUiThread(() -> {
+                activity.textViewQuote.setText(quote);
+                activity.textViewAuthor.setText(author);
+            });
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lab_10/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.lab_10;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textViewQuote;
+    TextView textViewAuthor;
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textViewQuote = findViewById(R.id.textViewQuote);
+        textViewAuthor = findViewById(R.id.textViewAuthor);
+        button = findViewById(R.id.button);
+
+        ApiThread apiThread = new ApiThread(this);
+        apiThread.start();
+        button.setOnClickListener(v -> {
+            ApiThread thread = new ApiThread(this);
+            thread.start();
+        });
+    }
+}
